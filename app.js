e6197010338864c9c2cdc47faea30c4781be7d9e
Cory Hisey <cjhisey@gmail.com>
2020-04-24 17:47:41 -0700

    adding all the pages

--- app.js
+++ app.js
@@ -8,6 +8,36 @@ app.get('/', (req, res) => {
     res.render('landing');
 })
 
+app.get('/blog', (req, res) => {
+    res.render('blog');
+})
+
+app.get('/3d', (req, res) =>{
+    res.render('3d');
+})
+
+app.get('/map', (req, res) =>{
+    res.render('map');
+})
+
+app.get('/finance', (req, res) =>{
+    res.render('finance');
+})
+
+app.get('/marrakech', (req, res) =>{
+    res.render('marrakech');
+})
+
+app.get('/smallposts', (req, res) =>{
+    res.render('smallposts');
+})
+
+app.get('/photogallery', (req, res) => {
+    res.render('photogallery');
+})
+app.use(express.static(process.env.PWD + '/public'));
+
+app.use(express.static(__dirname + '/public'));
 
 const PORT = process.env.PORT || 3000;
 
